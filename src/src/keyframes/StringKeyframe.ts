c2b76479526c875d00fb2088c6550a4490650ec0
Robert May <robertmay2003@gmail.com>
2020-09-14 14:41:46 -0400

    Fix string keyframe interpolation

--- src/src/keyframes/StringKeyframe.ts
+++ src/src/keyframes/StringKeyframe.ts
@@ -3,30 +3,26 @@ import Keyframe from './Keyframe';
 import ListKeyframe from './ListKeyframe';
 
 class StringKeyframe extends Keyframe<string> {
-    protected listKeyframe: ListKeyframe;
+  constructor(
+    time: number,
+    value: string,
+    inEasing: Easing = Easing.cubic,
+    outEasing: Easing | undefined = undefined,
+  ) {
+    super(time, value, inEasing, outEasing);
+  }
 
-    constructor(
-      time: number,
-      value: string,
-      inEasing: Easing = Easing.cubic,
-      outEasing: Easing | undefined = undefined,
-    ) {
-      super(time, value, inEasing, outEasing);
+  interpolate(keyframe: StringKeyframe, time: number): string {
+    return this.toListKeyframe()
+      .interpolate(keyframe.toListKeyframe(), time)
+      .map((n) => String.fromCharCode(Math.round(n)))
+      .join('');
+  }
 
-      this.listKeyframe = this.toListKeyframe();
-    }
-
-    interpolate(keyframe: StringKeyframe, time: number): string {
-      return this.listKeyframe
-        .interpolate(keyframe.listKeyframe, time)
-        .map((n) => String.fromCharCode(Math.round(n)))
-        .join('');
-    }
-
-    protected toListKeyframe(): ListKeyframe {
-      const floatList = this.value.split('').map((c) => c.charCodeAt(0));
-      return new ListKeyframe(this.time, floatList, this.inEasing, this.outEasing);
-    }
+  protected toListKeyframe(): ListKeyframe {
+    const floatList = this.value.split('').map((c) => c.charCodeAt(0));
+    return new ListKeyframe(this.time, floatList, this.inEasing, this.outEasing);
+  }
 }
 
 export default StringKeyframe;
